7557158c9416b49888cd9d46b3f5be716f836003
Breno <Breno>
2022-02-08 20:43:02 -0300

    Finalizado!

--- src/com/company/dados/OrdenaLista.java
+++ src/com/company/dados/OrdenaLista.java
@@ -2,11 +2,28 @@ package com.company.dados;
 
 import com.company.produto.Produto;
 
-import java.util.Date;
+import java.util.*;
 
-public class OrdenaLista {
+public class OrdenaLista{
     Lista listaProdutos = new Lista();
     Date data;
+
+    static class ProdutoItem {
+        Produto produto;
+
+        ProdutoItem(Produto produto) {
+            this.produto = produto;
+        }
+    }
+
+    static class SortByDate implements Comparator<ProdutoItem> {
+        @Override
+        public int compare(ProdutoItem a, ProdutoItem b) {
+            return a.produto.getDataDeCompra().compareTo(b.produto.getDataDeCompra());
+        }
+    }
+
+
     public void convertDate(){
         for (Produto produto: listaProdutos.getListaDeProdutos()) {
             data = new Date();
@@ -17,8 +34,18 @@ public class OrdenaLista {
 
     public void ordenarLista(){
         this.convertDate();
+        ArrayList<ProdutoItem> listaOrdenada = new ArrayList<>();
         for (Produto produto: listaProdutos.getListaDeProdutos()) {
-            System.out.println(produto.getDataComparar());
+            listaOrdenada.add(new ProdutoItem(produto));
+        }
+        Collections.sort(listaOrdenada, new SortByDate());
+        System.out.println("Lista ordenada: ");
+
+        for (ProdutoItem p: listaOrdenada) {
+            System.out.println(p.produto.toString());
+            System.out.println();
         }
     }
 }
+
+
